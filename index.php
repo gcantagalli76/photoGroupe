7251c5676c58825dc3f0f1a7c65523ccb2189d8b
PaulBoulier <lhp6.pboulier@gmail.com>
2021-06-21 14:11:02 +0200

    index logic

--- index.php
+++ index.php
@@ -1,26 +1,43 @@
+<?php
+
+if (!empty($_POST) && isset($_POST["login"])) {
+    $valid_mail = filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL);
+    $passwordsList = json_decode("./assets/json/members.json");
+    if (!$passwordsList) {
+        $valid_pass = isset($_POST["password"]);
+    }
+
+    if ($valid_mail && $valid_pass) {
+        session_start(["mail" => $_POST["mail"], "password" => $_POST["password"]]);
+        header("Location: ./gallery.php");
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en" class="home">
-<head>
+<head class="home">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link href="./assets/css/style.css" rel="stylesheet">
     <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
-<body >
-    
+<body>
+
 <div class="container-fluid text-center h-100 align-items-center">
 
-<div class="row justify-content-center">
+<div class="row justify-content-center h-100 align-items-center">
       <div class="col-sm-3 bg-light border">
         <h1>Connectez-vous à votre compte</h1>
 
-        <label class="form-label mt-4 d-flex justify-content-start"> Logo :</label>
-        <input type="email" class="form-control box">
+        <label class="form-label mt-4 d-flex justify-content-start" name>Adresse e-mail :</label>
+        <input type="email" class="form-control box" name="mail">
 
         <label class="form-label mt-2 d-flex justify-content-start">Mot de passe :</label>
-        <input type="password" class="form-control box">
+        <input type="password" class="form-control box" name="password">
 
         <button type="submit" class="btn text-white bg-primary mt-3">Se connecter</button>
 
