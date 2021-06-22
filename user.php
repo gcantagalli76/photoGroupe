30214409476bbe5244097888b700bb28cdaafda3
Gianni Cantagalli <gcantagalli76@gmail.com>
2021-06-22 15:02:00 +0200

    recup js

--- user.php
+++ user.php
@@ -7,6 +7,32 @@ if (!empty($_POST) && isset($_POST["logout"]) && !empty($_SESSION)) {
     exit();
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+
 <form action="./user.php" method="POST">
     <button type="submit" name="logout">Déconnexion</button>
 </form>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
