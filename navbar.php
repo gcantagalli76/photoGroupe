94b6ea31f965fd7094da01138d35d013069ab15f
PaulBoulier <lhp6.pboulier@gmail.com>
2021-06-23 13:49:45 +0200

    script details

--- navbar.php
+++ navbar.php
@@ -1,13 +1,13 @@
 <div class="row footer-size align-items-center fixed-bottom mt-5 navbar">
-    <div class="col-sm-4 d-flex flex-column align-items-center">
+    <div class="col-4 d-flex flex-column align-items-center">
     <div class="text-center">Accueil</div>
         <a href="gallery.php"><img src="./assets/img/house-door.svg" class="" alt="house" width="30px" type="button"></a>
     </div>
-    <div class="col-sm-4 d-flex flex-column align-items-center">
+    <div class="col-4 d-flex flex-column align-items-center">
         <div class="text-center">Nouvel ajout</div>
         <a href="upload.php"><img src="./assets/img/file-plus.svg" class="" alt="plus" width="30px"></a>
     </div>
-    <div class="col-sm-4 d-flex flex-column align-items-center">
+    <div class="col-4 d-flex flex-column align-items-center">
         <div class="text-center">Profil</div>
         <a href="user.php"><img src="./assets/img/person.svg" class="" alt="person" width="30px"></a>
     </div>
